1f5671c751f4f936c0f7d93af87c565fe0dc4d23
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-10-25 21:40:33 +0900

    Q2 only left thing to do is NN recognition

--- q1_q5.py
+++ q1_q5.py
@@ -10,6 +10,7 @@ import numpy as np
 import seaborn as sns
 import itertools
 from matplotlib import pyplot as plt
+import time
 
 import scipy.io
 mat = scipy.io.loadmat('face.mat')
@@ -61,6 +62,13 @@ def show_graph(data):
 # plt.tight_layout()
 # plt.show()
 
+def sorted_eigens(cov_matrix):
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_indices]
+    sorted_eigenvec = eigenvectors[:, sorted_indices]
+    return sorted_eigenvalues, sorted_eigenvec
 
 def recons(inp_norm, eigen_vec, top_m):
     weights = np.dot(inp_norm, eigen_vec[:, :top_m])
@@ -106,6 +114,8 @@ def make_2pix_feat(ori_vec):
     return np.array(result_arrays)
 
 # The mean (avg) image
+
+start_time = time.time()
 mean_img = train_flat.mean(axis=0)
     
 A_vec = train_flat - mean_img[np.newaxis, :]
@@ -116,13 +126,14 @@ A_vec = train_flat - mean_img[np.newaxis, :]
 # Way 2
 cov_matrix = A_vec.T.dot(A_vec)/len(A_vec)
 
-eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
-eigenvalues, eigenvectors = np.real(eigenvalues), np.real(eigenvectors)
+sorted_eigenvalues, sorted_eigenvec = sorted_eigens(cov_matrix)
+# eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+# # eigenvalues, eigenvectors = np.real(eigenvalues), np.real(eigenvectors)
 
-# Sort eigenvalues and corresponding eigenvectors in descending order
-sorted_indices = np.argsort(eigenvalues)[::-1]
-sorted_eigenvalues = eigenvalues[sorted_indices]
-sorted_eigenvec = eigenvectors[:, sorted_indices]
+# # Sort eigenvalues and corresponding eigenvectors in descending order
+# sorted_indices = np.argsort(eigenvalues)[::-1]
+# sorted_eigenvalues = eigenvalues[sorted_indices]
+# sorted_eigenvec = eigenvectors[:, sorted_indices]
 
 
 # Count the number of positive elements
@@ -148,30 +159,34 @@ train_projs = [np.dot(norm_img, sorted_eigenvec[:, :top_m])
 test_norm = test_flat - mean_img[np.newaxis, :]
 test_projs = [np.dot(norm_img, sorted_eigenvec[:, :top_m])
               for norm_img in test_norm]
+end_time = time.time()
+print(f'Time elapsed (Batch PCA): {end_time-start_time}s')
 # test_2pixS = [make_2pix_feat(vec) for vec in test_projs]
 
 
 # reconstruct for all train set, calc recon error.
 
-# train_recon = [mean_img + recons(norm_img, sorted_eigenvec, top_m)
-#                for norm_img in A_vec]
-# train_rec_err = np.mean(np.linalg.norm(train_flat-train_recon, ord=2, axis=1))
+train_recon = [mean_img + recons(norm_img, sorted_eigenvec, top_m)
+               for norm_img in A_vec]
+train_rec_err = np.mean(np.linalg.norm(train_flat-train_recon, ord=2, axis=1))
+# show_arr_imgs(train_recon[:5])
 
-
-# learn KNN Classifier using train data
+# # learn KNN Classifier using train data
+# n_nb = 3
 # neigh = KNeighborsClassifier(n_nb, weights='distance', metric='cosine')
 # neigh.fit(train_projs, train_y)
 
-# Predict with KNN
+# # Predict with KNN
 # test_pred = neigh.predict(test_projs)
 # test_acc = accuracy_score(test_y, test_pred)
 # print('KNN test acc: ', test_acc)  # M = 100, K = 3 gave best acc.
 # print(precision_recall_fscore_support(test_y, test_pred, average='macro'))
 
-# Calculate the confusion matrix
+# # Calculate the confusion matrix
 # confusion_matrix = confusion_matrix(test_y, test_pred)
+# show_conf_mat(confusion_matrix)
 
-'''
+
 ### Q2 - Incremental PCA
 
 # Split training data into n_sub subsets
@@ -179,23 +194,39 @@ n_sub = 4
 train_subsets = np.array(np.array_split(train_flat, n_sub))
 
 models = []
+print('Training incremental PCA models...')
+start_time = time.time()
 for i in range(n_sub):
+    print(f'Training model {i+1}...')
     subset_mean = train_subsets[i].mean(axis=0)
     subset_N = len(train_subsets[i])
 
     subset_A_vec = train_subsets[i] - subset_mean[np.newaxis, :]
     subset_cov_matrix = subset_A_vec.T.dot(subset_A_vec)/subset_N
-    subset_eigenvalues, subset_eigenvectors = np.linalg.eig(subset_cov_matrix)
-    subset_eigenvalues, subset_eigenvectors = np.real(subset_eigenvalues), np.real(subset_eigenvectors)
+    # subset_eigenvalues, subset_eigenvectors = np.linalg.eigh(subset_cov_matrix)
 
-    subset_sorted_indices = np.argsort(subset_eigenvalues)[::-1]
-    subset_sorted_eigenvalues = subset_eigenvalues[subset_sorted_indices]
-    subset_sorted_eigenvec = subset_eigenvectors[:, subset_sorted_indices]
+    # subset_sorted_indices = np.argsort(subset_eigenvalues)[::-1]
+    # subset_sorted_eigenvalues = subset_eigenvalues[subset_sorted_indices]
+    # subset_sorted_eigenvec = subset_eigenvectors[:, subset_sorted_indices]
 
-    models.append((subset_mean, subset_N, subset_sorted_eigenvec, subset_sorted_eigenvalues))
+    models.append((subset_mean, subset_N, subset_cov_matrix))
+    if i==0:
+        pause_time = time.time()
+
+incremental_models = [models[0]]
+
+for i in range(n_sub-1):
+    print(f'Incrementing model {i+1}...')
+    inc_N = incremental_models[-1][1] + models[i+1][1]
+    inc_mean = (incremental_models[-1][0]*incremental_models[-1][1] + models[i+1][0]*models[i+1][1])/inc_N
+    inc_cov_matrix = incremental_models[-1][0]/inc_mean * incremental_models[-1][2] + models[i+1][0]/inc_mean * models[i+1][2] + \
+            (incremental_models[-1][1]*models[i+1][1]/inc_N**2) * (incremental_models[-1][0]-models[i+1][0]) * (incremental_models[-1][0]-models[i+1][0]).T
+    incremental_models.append((inc_mean, inc_N, inc_cov_matrix))
 
 # models[0] is PCA trained only by the first subset:
-s1_mean, s1_N, s1_sorted_eigenvec, s1_sorted_eigenvalues = models[0]
+resume_time = time.time()
+s1_mean, s1_N, s1_cov_matrix = models[0]
+s1_sorted_eigenvalues, s1_sorted_eigenvec = sorted_eigens(s1_cov_matrix)
 s1_A_vec = train_subsets[0] - s1_mean[np.newaxis, :]
 s1_train_projs = [np.dot(norm_img, s1_sorted_eigenvec[:, :top_m])
                for norm_img in s1_A_vec]
@@ -204,14 +235,42 @@ s1_train_projs = [np.dot(norm_img, s1_sorted_eigenvec[:, :top_m])
 s1_test_norm = test_flat - s1_mean[np.newaxis, :]
 s1_test_projs = [np.dot(norm_img, s1_sorted_eigenvec[:, :top_m])
               for norm_img in s1_test_norm]
+s1_end_time = time.time()
+print(f'Time elapsed (1st subset PCA): {s1_end_time-resume_time+pause_time-start_time}s')
 
 # print(s1_test_projs[0].shape, len(s1_test_projs))
 
 s1_train_recon = [s1_mean + recons(norm_img, s1_sorted_eigenvec, top_m)
                for norm_img in s1_A_vec]
-train_rec_err = np.mean(np.linalg.norm(train_subsets[0]-s1_train_recon, ord=2, axis=1))
-show_arr_imgs(s1_train_recon[:5])
-'''
+s1_train_rec_err = np.mean(np.linalg.norm(train_subsets[0]-s1_train_recon, ord=2, axis=1))
+# show_arr_imgs(s1_train_recon[:5])
+
+
+# incremental_models[-1] is PCA trained by all subsets:
+inc_mean, inc_N, inc_cov_matrix = incremental_models[-1]
+inc_sorted_eigenvalues, inc_sorted_eigenvec = sorted_eigens(inc_cov_matrix)
+inc_A_vec = train_flat - inc_mean[np.newaxis, :]
+inc_train_projs = [np.dot(norm_img, inc_sorted_eigenvec[:, :top_m])
+               for norm_img in inc_A_vec]
+
+inc_test_norm = test_flat - inc_mean[np.newaxis, :]
+inc_test_projs = [np.dot(norm_img, inc_sorted_eigenvec[:, :top_m])
+              for norm_img in inc_test_norm]
+end_time = time.time()
+print(f'Time elapsed (Incremental PCA): {end_time-start_time}s')
+
+inc_train_recon = [inc_mean + recons(norm_img, inc_sorted_eigenvec, top_m)
+                for norm_img in inc_A_vec]
+inc_train_rec_err = np.mean(np.linalg.norm(train_flat-inc_train_recon, ord=2, axis=1))
+# show_arr_imgs(inc_train_recon[:5])
+
+print("Reconstruction errors:")
+print(f'Batch PCA: {train_rec_err}')
+print(f'1st subset PCA: {s1_train_rec_err}')
+print(f'Incremental PCA: {inc_train_rec_err}')
+# I'm not sure why incremental PCA has higher reconstruction error than batch PCA, and why is error in 1st subset PCA so small
+
+
 
 # Random Forest. Then learn to fit
 rfc = RandomForestClassifier(n_estimators=360, 
